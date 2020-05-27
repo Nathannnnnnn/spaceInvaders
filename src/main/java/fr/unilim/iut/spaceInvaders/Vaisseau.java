9b128a2a856689b14bea0e2fdc0c292c8a56ddf1
Nathan.Verdeyme <nathan.verdeyme@etu.unilim.fr>
2020-05-27 16:56:43 +0200

    Initial commit

---
+++ src/main/java/fr/unilim/iut/spaceInvaders/Vaisseau.java
@@ -0,0 +1,27 @@
+package fr.unilim.iut.spaceInvaders;
+
+public class Vaisseau {
+
+	int x;
+	int y;
+
+	public Vaisseau(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public boolean occupeLaPosition(int x, int y) {
+		return (this.x == x) && (this.y == y);
+	}
+
+	public void seDeplacerVersLaDroite() {
+		this.x += 1;
+	}
+	public void seDeplacerVersLaGauche() {
+		this.x -= 1;
+	}
+
+	public int abscisse() {
+		return this.x;
+	}
+}
---
+++ src/main/java/fr/unilim/iut/spaceInvaders/utils/HorsEspaceJeuException.java
@@ -0,0 +1,10 @@
+package fr.unilim.iut.spaceInvaders.utils;
+
+@SuppressWarnings("serial")
+public class HorsEspaceJeuException extends RuntimeException {
+
+    public HorsEspaceJeuException(String message) {
+	    super(message);
+    }
+
+}
